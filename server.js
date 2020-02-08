8ba2d52d2b337bbf572b4ffae1c5a3d396323556
LoesJanssen <loesjanssen89@gmail.com>
2020-02-08 10:08:30 +0100

    Insall body-parser, add post server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/message", (req, res) => {
+  console.log(req.body);
+  res.json({
+    message: "We received your request body!"
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
