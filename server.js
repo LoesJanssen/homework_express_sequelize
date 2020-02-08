ee86878e045e481e25cca4006e80f5cdff1a2cff
LoesJanssen <loesjanssen89@gmail.com>
2020-02-08 13:09:33 +0100

    one endpoint messages in server.js

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const messageValidatorMiddleware = (req, res, next) => {
+  if (req.body.message === "") {
+    res.status(400).json({
+      message: "Bad request"
+    });
+  } else if (req.body.message.id > 5) {
+    res.status(429).json({
+      message: "Too many requests"
+    });
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => res.send("Hello World!"));
-app.post("/message", (req, res) => {
+
+app.post("/messages", messageValidatorMiddleware, (req, res) => {
   console.log(req.body);
-  res.json({
-    message: "We received your request body!"
-  });
+  res.send({ message: "This is the message that was send" + req.body.message });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
